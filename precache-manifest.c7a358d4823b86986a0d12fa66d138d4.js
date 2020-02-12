33784affdaa6695a264791a8bc118c0ae975067f
Serg <bloodfast13@gmail.com>
2020-02-12 10:28:10 +0200

    Updates

---
+++ precache-manifest.c7a358d4823b86986a0d12fa66d138d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859a07edad061aceb884f36eed93b5a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc4c336988a7051d9302",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "600feb7e733e2f83972b",
+    "url": "./static/js/2.7abd8533.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.7abd8533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc4c336988a7051d9302",
+    "url": "./static/js/main.11dd4497.chunk.js"
+  },
+  {
+    "revision": "f9a752b41214259e92c4",
+    "url": "./static/js/runtime-main.39dcb93e.js"
+  }
+]);
